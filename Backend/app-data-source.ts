f58e188700b2a87780a29feba3834df50e1a0100
Yunadi <yunadifitra@gmail.com>
2023-12-16 17:15:53 +0700

    Perubahan env ke database

--- Backend/app-data-source.ts
+++ Backend/app-data-source.ts
@@ -1,4 +1,5 @@
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
 import { User } from "./src/entity/user.entity";
 import { Token } from "./src/entity/token.entity";
 import { Food } from "./src/entity/food.entity";
@@ -7,14 +8,15 @@ import { Diet } from "./src/entity/diet.entity";
 import { Workout } from "./src/entity/workout.entity";
 // import { Food } from "./src/entity/food.entity"
 // import { Consumption } from "./src/entity/consumption.entity"
+dotenv.config();
 
 export const myDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: process.env.db_host,
   port: 3306,
-  username: "root",
-  password: "",
-  database: "capstone",
+  username: process.env.db_user,
+  password: process.env.db_password,
+  database: process.env.db_name,
   entities: [User, Token, Food, Consumption, Diet, Workout],
   logging: true,
   synchronize: false,
